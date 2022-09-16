4fa4f4c6783fd58358353920d4c7bf5290584d61
Revathi Mahalingam <103559154+revathimsc@users.noreply.github.com>
2022-09-16 17:04:38 +0530

    Add files via upload

---
+++ model/userSchema.js
@@ -0,0 +1,52 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true,
+        trim: true,
+        min: 5,
+        max: 20
+    },
+    lastname: {
+        type: String,
+        required: true,
+        trim: true,
+        min: 5,
+        max: 20
+    },
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        index: true,
+        lowercase: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String
+    }
+});
+
+userSchema.pre('save', async function(next){
+
+    if(!this.isModified('password')){
+        next()
+    }
+    this.password= await bcrypt.hash(this.password,10);
+});
+
+const user = mongoose.model('user', userSchema);
+export default user;
